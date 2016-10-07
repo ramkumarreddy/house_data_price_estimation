1d5db5625fc59507a89ac34718d63c499803f090
ramkumar <ramkumarkovvuri>
2016-10-07 10:52:35 +0530

    Changed combination of regressors - 0.12528

--- change.py
+++ change.py
@@ -14,5 +14,21 @@ for i in range(0,len(content)):
 			x = 0.2
 			y = 0.2
 			z = 0.6
+		elif abs(c-a)<6100:
+			x = 0.25
+			y = 0.25
+			z = 0.5
+		elif abs(c-a)<16000:
+			x = 0.3
+			y = 0.3
+			z = 0.4
+		elif abs(c-a)<20000:
+			x = 0.35
+			y = 0.45
+			z = 0.2
+		elif abs(c-a)<33000:
+			x = 0.45
+			y = 0.45
+			z = 0.1
 		ans = (a*x)+(b*y)+(c*z)
 		print "%d,%f" %(i+1461,ans)
